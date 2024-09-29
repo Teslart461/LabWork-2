9ee9f892352c20974aab177010a2ff22bacef55c
Teslart461 <apanteleev1001@gmail.com>
2024-09-30 00:22:03 +0700

    Added 2 functions
    Добавлено 2 функции, осталось ещё 6

--- Lab_3 2.cpp
+++ Lab_3 2.cpp
@@ -5,3 +5,16 @@
 #include <Windows.h>
 #include "MyStruct.h"
 
+// Функция для подсчета калорий одного ингредиента
+double calculate_cal(Ingredient ingredient) {
+    return (ingredient.cal_per_100g * ingredient.weight_in_grams) / 100;
+}
+
+// Функция для подсчета калорий блюда
+double calculate_dish_calories(Dish dish) {
+    double dish_calories = 0;
+    for (int i = 0; i < dish.num_ingredients; i++) {
+        dish_calories += calculate_cal(dish.ingredients[i].ingredient);
+    }
+    return dish_calories;
+}
