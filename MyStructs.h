cf63dce331cecf38b50c4f5bd248d9c56e8e2e07
Teslart461 <apanteleev1001@gmail.com>
2024-09-29 23:23:22 +0700

    All structures have been made

--- MyStructs.h
+++ MyStructs.h
@@ -18,14 +18,40 @@ typedef struct {
 
 // Структура используемая для ингредиентов в случае, если необходима пищевая ценность этого продукта
 typedef struct {
-    Ingredient ingredient;       // Ингредиент
-    NutritionFacts nutrition;    // Пищевая ценность на 100 грамм
+    Ingredient ingredient;       
+    NutritionFacts nutrition;    
 } DetailedIngredient;
 
 // Структура для представления блюда
 typedef struct {
-    char name[50];                // Название блюда
-    DetailedIngredient ingredients[20];  // Массив ингредиентов
-    int num_ingredients;          // Количество ингредиентов
+    char name[50];                
+    DetailedIngredient ingredients[20];  
+    int num_ingredients;          
 } Dish;
 
+// Структура для приёма пищи (завтрак, обед, ужин и т.д.)
+typedef struct {
+    char name[30];                
+    Dish dishes[20];             
+    int num_dishes;              
+} Meal;
+
+// Структура для меню
+typedef struct {
+    Meal maels[5];  
+    int num_meals;          
+} Menu;
+
+// Структура для представления пользователя
+typedef struct {
+    char name[50];                
+} User;
+
+// Структура для вывода результата подсчета калорий
+typedef struct {
+    User user;                    
+    Menu menu;                    
+    double total_cal;          
+} CalReport;
+
+#endif // MYSTRUCTS_H
