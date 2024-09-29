f8209bced220fb8599d9f863b02b374c33054277
Teslart461 <apanteleev1001@gmail.com>
2024-09-29 23:05:49 +0700

    Added 4 main structures
    Добавил 4 основные структуры. Надо будет обдумать. Добавлять ли ещё 4, которые уже будут дополнительны, но не излишни (возможно)

--- MyStructs.h
+++ MyStructs.h
@@ -1,3 +1,31 @@
 #ifndef MYSTRUCTS_H
 #define MYSTRUCTS_H
 
+// Структура для представления ингредиента
+typedef struct {
+    char name[50];             
+    double cal_per_100g;     
+    double weight_in_grams;       
+} Ingredient;
+
+// Структура для представления пищевой ценности
+typedef struct {
+    double proteins;  
+    double fats;      
+    double carbs;     
+    double fiber;     
+} NutritionFacts;
+
+// Структура используемая для ингредиентов в случае, если необходима пищевая ценность этого продукта
+typedef struct {
+    Ingredient ingredient;       // Ингредиент
+    NutritionFacts nutrition;    // Пищевая ценность на 100 грамм
+} DetailedIngredient;
+
+// Структура для представления блюда
+typedef struct {
+    char name[50];                // Название блюда
+    DetailedIngredient ingredients[20];  // Массив ингредиентов
+    int num_ingredients;          // Количество ингредиентов
+} Dish;
+
