48e3d4ce62dfd3bf7da9dc319336231560a4f486
Teslart461 <apanteleev1001@gmail.com>
2024-09-30 20:24:09 +0700

    Первый работающий экземпляр
    Был написан во время большого перерыва  университете, необходимо доработать на станке

--- Lab_3 2.cpp
+++ Lab_3 2.cpp
@@ -36,3 +36,93 @@ double calculate_menu_calories(Menu* menu) {
     }
     return menu_cal;
 }
+
+void generate_report(User* user, Menu* menu) {
+    printf("Отчет о калориях для пользователя: %s\n", user->name);
+    printf("-------------------------------------------------\n");
+
+    // Проходим по каждому приему пищи
+    for (int i = 0; i < menu->num_meals; i++) {
+        Meal* meal = &menu->meals[i];
+        double meal_cal = calculate_meal_calories(meal);  // Используем функцию для подсчета калорий приема пищи
+
+        printf("Прием пищи: %s\n", meal->name);
+        printf("Калорийность приема пищи: %.2f калорий\n", meal_cal);
+
+        // Проходим по каждому блюду в приеме пищи
+        for (int j = 0; j < meal->num_dishes; j++) {
+            Dish* dish = &meal->dishes[j];
+            double dish_cal = calculate_dish_calories(dish);  // Используем функцию для подсчета калорий блюда
+
+            printf("  Блюдо: %s, Калорийность: %.2f калорий\n", dish->name, dish_cal);
+        }
+        printf("-------------------------------------------------\n");
+    }
+
+    // Подсчитываем и выводим общую калорийность всего меню
+    double total_calories = calculate_menu_calories(menu);  // Используем функцию для подсчета калорий меню
+    printf("Общие калории за весь день: %.2f калорий\n", total_calories);
+}
+
+void input_ingredient(DetailedIngredient* ingredient) {
+    printf("Введите название ингредиента: ");
+    scanf("%s", ingredient->ingredient.name);
+
+    printf("Введите калории на 100 грамм: ");
+    scanf("%lf", &ingredient->ingredient.cal_per_100g);
+
+    printf("Введите вес ингредиента в граммах: ");
+    scanf("%lf", &ingredient->ingredient.weight_in_grams);
+}
+
+void input_dish(Dish* dish) {
+    printf("Введите название блюда: ");
+    scanf("%s", dish->name);
+
+    printf("Введите количество ингредиентов в блюде: ");
+    scanf("%d", &dish->num_ingredients);
+
+    for (int i = 0; i < dish->num_ingredients; i++) {
+        printf("Ингредиент %d:\n", i + 1);
+        input_ingredient(&dish->ingredients[i]);
+    }
+}
+
+void input_meal(Meal* meal) {
+    printf("Введите название приема пищи: ");
+    scanf("%s", meal->name);
+
+    printf("Введите количество блюд на этом приеме пищи: ");
+    scanf("%d", &meal->num_dishes);
+
+    for (int i = 0; i < meal->num_dishes; i++) {
+        printf("Блюдо %d:\n", i + 1);
+        input_dish(&meal->dishes[i]);
+    }
+}
+
+void input_menu(Menu* menu) {
+    printf("Введите количество приемов пищи в меню: ");
+    scanf("%d", &menu->num_meals);
+
+    for (int i = 0; i < menu->num_meals; i++) {
+        printf("Прием пищи %d:\n", i + 1);
+        input_meal(&menu->meals[i]);
+    }
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    User user;
+    Menu menu;
+
+    printf("Введите имя пользователя: ");
+    scanf("%s", user.name);
+
+    input_menu(&menu);
+
+    generate_report(&user, &menu);
+
+}
