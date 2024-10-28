1289c53550562caa30c14191b5a6162d3b8c2f24
Teslart461 <apanteleev1001@gmail.com>
2024-10-28 14:16:13 +0700

    Working var Lab 3.3.cpp
    Рабочий вариант

--- Lab 3.3.cpp
+++ Lab 3.3.cpp
@@ -8,5 +8,31 @@ int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
+    // Создаем пользователя
+    User user;
+    cout << "Введите данные пользователя:\n";
+    user.input();
+    cout << "Привет, " << user.getName() << "!\n";
+
+    // Создаем меню
+    Menu menu;
+    cout << "Создание меню:\n";
+    menu.input();
+
+    // Вычисляем общее количество калорий в меню
+    double totalCalories = menu.calculateCalories();
+    cout << "Общее количество калорий в меню: " << totalCalories << " ккал\n";
+
+    // Ввод данных о физической активности
+    PhysicalActivity activity;
+    cout << "Введите данные о физической активности:\n";
+    activity.input();
+    double caloriesBurned = activity.getCaloriesBurned();
+    cout << "Сожжено калорий: " << caloriesBurned << " ккал\n";
+
+    // Вычисляем баланс калорий
+    double netCalories = totalCalories - caloriesBurned;
+    cout << "Итоговый баланс калорий: " << netCalories << " ккал\n";
+
 return 0:
 }
