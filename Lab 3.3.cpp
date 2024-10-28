3dd94690bdff721ccfdd408f61ae0503c712e3c4
Teslart461 <apanteleev1001@gmail.com>
2024-10-28 20:45:51 +0700

    Finish Lab 3.3.cpp
    Представлена работа с динамическим массивом объектов класса и с массивом динамических объектов класса; применение операторов new, delete, delete[]; использование (*a) и ->

--- Lab 3.3.cpp
+++ Lab 3.3.cpp
@@ -1,38 +1,75 @@
 #include "MyStructs.h"
 #include <iostream>
-#include <Windows.h>
+#include <limits>
+#include <locale.h>
 
 using namespace std;
 
-int main() {
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
+    int main() {
+        setlocale(LC_ALL, "RUS");
 
-    // Создаем пользователя
-    User user;
-    cout << "Введите данные пользователя:\n";
-    user.input();
-    cout << "Привет, " << user.getName() << "!\n";
+        // Создаем пользователя
+        User user;
+        cout << "Введите данные пользователя:\n";
+        user.input();
+        cout << "Привет, " << user.getName() << "!\n";
 
-    // Создаем меню
-    Menu menu;
-    cout << "Создание меню:\n";
-    menu.input();
+        cout << "\nСейчас будет продемонстрирована работа с динамическим массив данных\n";
 
-    // Вычисляем общее количество калорий в меню
-    double totalCalories = menu.calculateCalories();
-    cout << "Общее количество калорий в меню: " << totalCalories << " ккал\n";
+        // Работа с динамическим массивом объектов класса Meal
+        int numMeals;
+        cout << "Введите количество приемов пищи: ";
+        while (!(cin >> numMeals)) {
+            cout << "Ошибка ввода. Пожалуйста, введите число.\n";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
 
-    // Ввод данных о физической активности
-    PhysicalActivity activity;
-    cout << "Введите данные о физической активности:\n";
-    activity.input();
-    double caloriesBurned = activity.getCaloriesBurned();
-    cout << "Сожжено калорий: " << caloriesBurned << " ккал\n";
+        // Создаем динамический массив объектов класса Meal
+        Meal* mealsArray = new Meal[numMeals];
+        for (int i = 0; i < numMeals; ++i) {
+            cout << "Введите информацию о приеме пищи " << (i + 1) << ":\n";
+            mealsArray[i].input();
+        }
 
-    // Вычисляем баланс калорий
-    double netCalories = totalCalories - caloriesBurned;
-    cout << "Итоговый баланс калорий: " << netCalories << " ккал\n";
+        // Подсчет калорий для каждого приема пищи
+        for (int i = 0; i < numMeals; ++i) {
+            cout << "Калории в приеме пищи " << (i + 1) << ": " << mealsArray[i].calculateCalories() << " ккал\n";
+        }
 
-return 0:
-}
+        // Удаление динамического массива объектов класса Meal
+        delete[] mealsArray;
+
+        cout << "\nСейчас будет продемонстрирована работа с массивом динамических данных\n";
+
+        // Работа с массивом динамических объектов класса Dish
+        int numDishes;
+        cout << "Введите количество блюд: ";
+        while (!(cin >> numDishes)) {
+            cout << "Ошибка ввода. Пожалуйста, введите целое число.\n";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+
+        // Создаем массив указателей на объекты класса Dish
+        Dish** dishesArray = new Dish * [numDishes];
+        for (int i = 0; i < numDishes; ++i) {
+            // Создаем каждый объект с помощью new
+            dishesArray[i] = new Dish();
+            cout << "Введите информацию о блюде " << (i + 1) << ":\n";
+            dishesArray[i]->input();
+        }
+
+        // Подсчет калорий для каждого блюда
+        for (int i = 0; i < numDishes; ++i) {
+            cout << "Калории в блюде " << (i + 1) << ": " << (*dishesArray[i]).calculateCalories() << " ккал\n";
+        }
+
+        // Удаление массива динамических объектов класса Dish
+        for (int i = 0; i < numDishes; ++i) {
+            delete dishesArray[i]; // Удаляем каждый объект
+        }
+        delete[] dishesArray; // Удаляем массив указателей
+
+        return 0;
+    }
