d0a2306740138a273b47056074c3f3bbcd276968
Teslart461 <apanteleev1001@gmail.com>
2024-12-16 00:36:37 +0700

    Update Lab 3_3.cpp and add work with Dish

--- Lab 3_3.cpp
+++ Lab 3_3.cpp
@@ -26,5 +26,20 @@ int main() {
     sampleIngredient.input();
     cout << "Проверка дружественной функции: " << sampleIngredient << endl;
 
+    // Пример работы с Dish (конструктор копии и оператор присваивания)
+    Dish dish1;
+    cout << "Введите данные для первого блюда:\n";
+    dish1.input();
+
+    Dish dish2 = dish1;  // Конструктор копии
+    cout << "Калорийность блюда dish2 (копия dish1): " << dish2.calculateCalories() << endl;
+
+    Dish dish3;
+    cout << "Введите данные для второго блюда:\n";
+    dish3.input();
+
+    dish3 = dish1;  // Оператор присваивания
+    cout << "Калорийность блюда dish3 (после присваивания dish1): " << dish3.calculateCalories() << endl;
+    
     return 0;
 }
