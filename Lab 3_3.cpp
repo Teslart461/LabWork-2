a41203fde69cb2eae94fa2b9bc12e49c5ea73ab8
Teslart461 <apanteleev1001@gmail.com>
2024-12-16 00:18:46 +0700

    Update Lab 3_3.cpp add dem-on friendly func for Ingredient

--- Lab 3_3.cpp
+++ Lab 3_3.cpp
@@ -6,20 +6,25 @@
 
 using namespace std;
 
-    int main() {
-        SetConsoleCP(1251);
-        SetConsoleOutputCP(1251);
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
-        // Создаем пользователя
-        User user;
-        cout << "Введите данные пользователя:\n";
-        user.input();
-        cout << "Привет, " << user.getName() << "!\n";
+    // Создаем пользователя
+    User user;
+    cout << "Введите данные пользователя:\n";
+    user.input();
+    cout << "Привет, " << user.getName() << "!\n";
 
-        Menu menu;
-        menu.input();
+    Menu menu;
+    menu.input();
 
-        cout << "Текущее количество меню: " << Menu::getMenuCount() << endl;
+    cout << "Текущее количество меню: " << Menu::getMenuCount() << endl;
 
-        return 0;
-    }
+    // Пример работы с Ingredient и дружественной функцией
+    Ingredient sampleIngredient;
+    sampleIngredient.input();
+    cout << "Проверка дружественной функции: " << sampleIngredient << endl;
+
+    return 0;
+}
