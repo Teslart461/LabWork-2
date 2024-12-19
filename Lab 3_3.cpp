372885ff7dff6a6fd61a22a68f9aad3c6635e08d
Teslart461 <apanteleev1001@gmail.com>
2024-12-19 21:34:27 +0700

    Delete unuse elements

--- Lab 3_3.cpp
+++ Lab 3_3.cpp
@@ -1,6 +1,5 @@
 #include "MyStructs.h"
 #include <iostream>
-#include <limits>
 #include <Windows.h>
 
 
@@ -16,46 +15,10 @@ int main() {
     user.input();
     cout << "Привет, " << user.getName() << "!\n";
 
+    // Создаем меню
     Menu menu;
+    cout << "Создание меню:\n";
     menu.input();
-
-    cout << "Текущее количество меню: " << Menu::getMenuCount() << endl;
-
-    // Пример работы с Ingredient и дружественной функцией
-    Ingredient sampleIngredient;
-    sampleIngredient.input();
-    cout << "Проверка дружественной функции: " << sampleIngredient << endl;
-
-    // Пример работы с Dish (конструктор копии и оператор присваивания)
-    Dish dish1;
-    cout << "Введите данные для первого блюда:\n";
-    dish1.input();
-
-    Dish dish2 = dish1;  // Конструктор копии
-    cout << "Калорийность блюда dish2 (копия dish1): " << dish2.calculateCalories() << endl;
-
-    Dish dish3;
-    cout << "Введите данные для второго блюда:\n";
-    dish3.input();
-
-    dish3 = dish1;  // Оператор присваивания
-    cout << "Калорийность блюда dish3 (после присваивания dish1): " << dish3.calculateCalories() << endl;
-
-    // Пример работы с DetailedIngredient и дружественной функцией
-    DetailedIngredient detailedIngredient;
-    cout << "Введите данные для детального ингредиента:\n";
-    detailedIngredient.input();
-    cout << "Проверка дружественной функции для DetailedIngredient: " << detailedIngredient << endl;
-    
-    // Исключение: вводим некорректные данные для проверки обработки
-    try {
-        cout << "Пример исключения:\n";
-        Menu invalidMenu;
-        invalidMenu.input();  // Введите отрицательное число для проверки
-    }
-    catch (const exception& e) {
-        cerr << "Обнаружено исключение: " << e.what() << endl;
-    }
     
     return 0;
 }
