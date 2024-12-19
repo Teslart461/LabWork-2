dcfa0fc575a3dbcfa5948689a20ddf9ea1f9fda5
Teslart461 <apanteleev1001@gmail.com>
2024-12-19 21:38:01 +0700

    Added previously used things)

--- Lab 3_3.cpp
+++ Lab 3_3.cpp
@@ -20,5 +20,17 @@ int main() {
     cout << "Создание меню:\n";
     menu.input();
     
+    double totalCalories = menu.calculateCalories();
+    cout << "Общее количество калорий в меню: " << totalCalories << " ккал\n";
+    
+    PhysicalActivity activity;
+    cout << "Введите данные о физической активности:\n";
+    activity.input();
+    double caloriesBurned = activity.getCaloriesBurned();
+    cout << "Сожжено калорий: " << caloriesBurned << " ккал\n";
+    
+    double netCalories = totalCalories - caloriesBurned;
+    cout << "Итоговый баланс калорий: " << netCalories << " ккал\n";
+    
     return 0;
 }
